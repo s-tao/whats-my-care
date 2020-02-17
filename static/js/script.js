af5593171bb4a06550edfe372fac8411549d2ed8
Sarah Tao <satao.94@gmail.com>
2020-02-17 02:44:28 +0000

    added all table info into jquery, shows on browser

--- static/js/script.js
+++ static/js/script.js
@@ -2,97 +2,23 @@
 
 
 
-// function iterMedPlan(res) {
-//   $('#plan-id-hdr').text('Plan ID');
-//   $('#carrier-name-hdr').text('Carrier');
-  
-//   console.log(res.length)
-
-//   // for (let i=0; i < res.length; i ++) {
-//     $('#display-name').html(res[i].display_name);
-//     $('#plan-id').html(res[i].id);
-//     $('#carrier-name').html(res[i].carrier_name);
-//   };
-
-// };
-
-// // function to display specific plan information
-// // function individualMedPlan(res) {
-// //   $('#plan-id-hdr').text('Plan ID');
-// //   $('#carrier-name-hdr').text('Carrier');
-  
-
-// //   $('#display-name').html(res[num].display_name);
-// //   $('#plan-id').html(res[num].id);
-// //   $('#carrier-name').html(res[num].user_options);
-  
-// // };
-
-// // function to get flask response to display individualPlan 
-// function showMedPlans(evt){
-//   evt.preventDefault();
-
-//   // let rowPlanType = "";
-//   // import user id instead of hard coding url
-
-//   // let url = `/users/${res[0].user_id}/show_plans`
-//   $('#display-plans').show();
-//   // $('#type-form').hide();
-
-//   $.get('/users/1/show_plans', (res) => {
-//     $('#plan-id-hdr').text('Plan ID');
-//     $('#carrier-name-hdr').text('Carrier');
-
-//     // const allPlans = res.map(plan => {
-//     //   $('#display-name').html(plan['display_name'].display_name);
-//     //   $('#plan-id').html(plan['id'].id);
-//     //   $('#carrier-name').html(plan['carrier_name'].carrier_name);
-
-//     // return allPlans
-//   $.get('/users/1/show_plans', individualMedPlan);
-//     });
-
-   
-//   });
-// };
-
-// //     $('#display-name').html(res[i].display_name);
-// //     $('#plan-id').html(res[i].id);
-// //     $('#carrier-name').html(res[i].carrier_name);
-
-// // table hidden until event listener activates when user submits
-// $('#display-plans').hide();
-// $('#type-form').on('submit', showMedPlans);
-
-// // create function to obtain user_id
-// // function getUserID() {
-// //   $.get()
-// // }
-
-
-
-
-// // $('#form-plan-group').on('submit', individualPlan, (evt) => {
-// //   evt.preventDefault();
-  
-
-
 // function to display specific plan information
 function individualMedPlan(medicalPlans) {
   
   // create variable to get html table id
   const indivPlanDiv = $('#indiv-plan')
-  console.log('indivPlan', indivPlan)
+  // console.log('indivPlan', indivPlanDiv)
 
   // create variable to add all plans in list
-  const tablePlans = [] 
+  const allTablePlans = [] 
 
   // loop through all plans
-  for(let i = 1; i <= 5; i++) {
+  for(let i = 1; i < medicalPlans.length; i++) {
     // create variable for each individual plan
     const planDetails = medicalPlans[i]
-    console.log('planDetails', planDetails)
+    // console.log('planDetails', planDetails)
 
+    // information needed per table
     const tablePlan = $(`\
     <table class="table table-hover table-sm"> \
       <thead> \
@@ -141,46 +67,26 @@ function individualMedPlan(medicalPlans) {
           <th scope="row" id="indiv-med-moop-hdr">Individual Max Out-of-Pocket</th> \
           <td id="plan-id">${planDetails.individual_medical_moop}</td> \
         </tr> \
+      </tbody> \
+    </table> \
+    `)
 
-
-      
-          `)
-
-    // tableData.push(tableRow[0])
-  }
-
-  // tablePlanBody.html(tableData)
-  // console.log('tableData', tableData)
-
-
-
-  // $('#plan-id-hdr').text('Plan ID');
-  // $('#carrier-name-hdr').text('Carrier');
-  
-
-  // $('#display-name').html(res[num].display_name);
-  // $('#plan-id').html(res[num].id);
-  // $('#carrier-name').html(res[num].carrier_name);
-  
+    allTablePlans.push(tablePlan)
+  };
+  indivPlanDiv.html(allTablePlans)
 };
 
-// function to get flask response to display individualPlan 
-function showMedPlans(evt){
+
+// tables hidden until event listener activates when user submits
+$('#display-plans').hide();
+
+// event listener to show tables and get data from server
+$('#type-form').on('submit', individualMedPlan, (evt) => {
   evt.preventDefault();
 
-  let rowPlanType = "";
-  // import user id instead of hard coding url
-
-  // let url = `/users/${res[0].user_id}/show_plans`
   $('#display-plans').show();
-  // $('#type-form').hide();
-
   $.get('/users/1/show_plans', individualMedPlan);
-};
-
-// table hidden until event listener activates when user submits
-$('#display-plans').hide();
-$('#type-form').on('submit', showMedPlans);
+});
 
 // create function to obtain user_id
 // function getUserID() {
@@ -188,23 +94,7 @@ $('#type-form').on('submit', showMedPlans);
 // }
 
 
-  let num = 0;
-  num ++
 
-
-
-// same action w.o functions
-//   $.get('/users/1/show_plans', (res) => {
-
-//     $('#display-name').html(res[num].display_name);
-//     $('#plan-id').html(res[num].id);
-//     $('#carrier-name').html(res[num].carrier_name);
-//   });
-// };
-
-// $('#form-plan-group').on('submit', individualPlan, (evt) => {
-//   evt.preventDefault();
-  
 
 
 
