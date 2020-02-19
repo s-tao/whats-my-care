da2ffe5ee24327b28cdb6b594bec2d38616ed0a2
Sarah Tao <satao.94@gmail.com>
2020-02-19 21:06:43 +0000

    slice testing works, create func btns

--- static/js/script.js
+++ static/js/script.js
@@ -1,77 +1,103 @@
 "use strict";
 
 let url = $(location).attr('href');
-let i = 1
+// set counter for slicing in for loop
+
+
+const hideMoreButton = () => {
+  $('#click-more-plans').hide();
+};
+
+const showMoreButton = () => {
+  $('#click-more-plans').show();
+};
+
+const hidePreviousButton = () => {
+  $('#click-previous-plans').hide();
+};
+
+const showPreviousButton = () => {
+  $('#click-previous-plans').show();
+};
 
 // function to display specific plan information
 function individualMedPlan(medicalPlans) {
   const allTablePlans = [] 
 
+  if (i > medicalPlans.length) {
+    hideMoreButton.call();
+    console.log(i, "no more plans to show");
+    console.log(medicalPlans.length, "length");
+  };
+
+  console.log(allTablePlans, "allTables");
   // let i = 1
 
   console.log(i,"i after post click")
   
   const displayPlanDiv = $('#display-plans-div');
-  console.log(medicalPlans)
-  console.log("i", i, "i+2", i+2)
   
-  for (let data in medicalPlans.slice(i, i+2)) {
+  // slice to only display a few plans 
+  const sliceCounter = medicalPlans.slice(i, i += 80);
 
-  const planDetails = medicalPlans[data];
+  for (let data in sliceCounter) {
+    console.log(i, "i")
+  
+    const planDetails = sliceCounter[data];
 
-  const tablePlan = $(`\
-    <div class="sub-container indiv-plan"> \
-      <table class="table table-hover table-sm" id="${planDetails.id}"> \
-        <thead> \
-          <tr> \
-            <th scope="col" id="display-name">${planDetails.display_name}</th> \
-          </tr> \
-        </thead> \
-        <tbody> \
-          <tr> \
-            <th scope="row" class="plan-id-hdr">Plan ID</th> \
-            <td class="plan-detail">${planDetails.id}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="carrier-hdr">Carrier</th> \
-            <td class="plan-detail">${planDetails.carrier_name}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="type-hdr">Plan Type</th> \
-            <td class="plan-detail">${planDetails.plan_type}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="pcp-hdr">Primary Care Physician</th> \
-            <td class="plan-detail">${planDetails.primary_care_physician}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="specialist-hdr">Specialist</th> \
-            <td class="plan-detail">${planDetails.specialist}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="er-hdr">Emergency Room</th> \
-            <td class="plan-detail">${planDetails.emergency_room}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="gen-drug-hdr">Generic Drug</th> \
-            <td class="plan-detail">${planDetails.generic_drugs}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="urg-care-hdr">Urgent Care</th> \
-            <td class="plan-detail">${planDetails.urgent_care}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="indiv-deduc-hdr">Individual Deductible</th> \
-            <td class="plan-detail">${planDetails.individual_medical_deductible}</td> \
-          </tr> \
-          <tr> \
-            <th scope="row" class="indiv-med-moop-hdr">Individual Max Out-of-Pocket</th> \
-            <td class="plan-detail">${planDetails.individual_medical_moop}</td> \
-          </tr> \
-        </tbody> \
-      </table> \
-    </div> \
-    `);
+    const tablePlan = $(`\
+      <div class="sub-container indiv-plan"> \
+        <table class="table table-hover table-sm" id="${planDetails.id}"> \
+          <thead> \
+            <tr> \
+              <th scope="col" id="display-name">${planDetails.display_name}</th> \
+            </tr> \
+          </thead> \
+          <tbody> \
+            <tr> \
+              <th scope="row" class="plan-id-hdr">Plan ID</th> \
+              <td class="plan-detail">${planDetails.id}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="carrier-hdr">Carrier</th> \
+              <td class="plan-detail">${planDetails.carrier_name}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="type-hdr">Plan Type</th> \
+              <td class="plan-detail">${planDetails.plan_type}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="pcp-hdr">Primary Care Physician</th> \
+              <td class="plan-detail">${planDetails.primary_care_physician}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="specialist-hdr">Specialist</th> \
+              <td class="plan-detail">${planDetails.specialist}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="er-hdr">Emergency Room</th> \
+              <td class="plan-detail">${planDetails.emergency_room}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="gen-drug-hdr">Generic Drug</th> \
+              <td class="plan-detail">${planDetails.generic_drugs}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="urg-care-hdr">Urgent Care</th> \
+              <td class="plan-detail">${planDetails.urgent_care}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="indiv-deduc-hdr">Individual Deductible</th> \
+              <td class="plan-detail">${planDetails.individual_medical_deductible}</td> \
+            </tr> \
+            <tr> \
+              <th scope="row" class="indiv-med-moop-hdr">Individual Max Out-of-Pocket</th> \
+              <td class="plan-detail">${planDetails.individual_medical_moop}</td> \
+            </tr> \
+          </tbody> \
+        </table> \
+      </div> \
+      `);
   
     allTablePlans.push(tablePlan);
   };
@@ -81,12 +107,11 @@ function individualMedPlan(medicalPlans) {
   $('#display-plans-div').show();
 
   // click button shows
-  $('#click-more-plans').show();
+  showMoreButton.call();
 };
 
 
 
-// find better way to replace tables w/o browser pushing up
 // tables hidden until event listener activates when user submits
 $('#display-plans-div').hide();
 
@@ -118,9 +143,9 @@ $('#click-more-plans').on('click', individualMedPlan, (evt) => {
 
 // potential form submittal for users to submit plan based off click
 // in the works
-$('table').on('click', () => {
-  alert("this works");
-});
+// $('table').on('click', () => {
+//   alert("this works");
+// });
 
 // $('table.table').on('click', (evt) => {
 //   alert('this works');
