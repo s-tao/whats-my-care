459031baa363a4269da356b98978bbaf2a54c568
Sarah Tao <satao.94@gmail.com>
2020-02-20 01:27:28 +0000

    moved tables into div inside form element

--- static/js/script.js
+++ static/js/script.js
@@ -33,7 +33,7 @@ function individualMedPlan(medicalPlans) {
     console.log(medicalPlans.length, "length");
   };
 
-  const displayPlanDiv = $('#display-plans-div');
+  const indivPlan = $('.indiv-plan');
   
   // slice to only display a few plans 
   const sliceCounter = medicalPlans.slice(i, i += 5);
@@ -43,72 +43,70 @@ function individualMedPlan(medicalPlans) {
     const planDetails = sliceCounter[data];
 
     const tablePlan = $(`\
-      <div class="sub-container indiv-plan"> \
-        <table class="table table-hover table-sm" id="${planDetails.id}"> \
-          <thead> \
-            <tr> \
-              <th scope="col" id="display-name">${planDetails.display_name}</th> \
-            </tr> \
-          </thead> \
-          <tbody> \
-            <tr> \
-              <th scope="row" class="plan-id-hdr">Plan ID</th> \
-              <td class="plan-detail">${planDetails.id}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="carrier-hdr">Carrier</th> \
-              <td class="plan-detail">${planDetails.carrier_name}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="type-hdr">Plan Type</th> \
-              <td class="plan-detail">${planDetails.plan_type}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="pcp-hdr">Primary Care Physician</th> \
-              <td class="plan-detail">${planDetails.primary_care_physician}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="specialist-hdr">Specialist</th> \
-              <td class="plan-detail">${planDetails.specialist}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="er-hdr">Emergency Room</th> \
-              <td class="plan-detail">${planDetails.emergency_room}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="gen-drug-hdr">Generic Drug</th> \
-              <td class="plan-detail">${planDetails.generic_drugs}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="urg-care-hdr">Urgent Care</th> \
-              <td class="plan-detail">${planDetails.urgent_care}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="indiv-deduc-hdr">Individual Deductible</th> \
-              <td class="plan-detail">${planDetails.individual_medical_deductible}</td> \
-            </tr> \
-            <tr> \
-              <th scope="row" class="indiv-med-moop-hdr">Individual Max Out-of-Pocket</th> \
-              <td class="plan-detail">${planDetails.individual_medical_moop}</td> \
-            </tr> \
+      <table class="table table-hover table-sm" id="${planDetails.id}"> \
+        <thead> \
+          <tr> \
+            <th scope="col" id="display-name">${planDetails.display_name}</th> \
+          </tr> \
+        </thead> \
+        <tbody> \
+          <tr> \
+            <th scope="row" class="plan-id-hdr">Plan ID</th> \
+            <td class="plan-detail">${planDetails.id}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="carrier-hdr">Carrier</th> \
+            <td class="plan-detail">${planDetails.carrier_name}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="type-hdr">Plan Type</th> \
+            <td class="plan-detail">${planDetails.plan_type}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="pcp-hdr">Primary Care Physician</th> \
+            <td class="plan-detail">${planDetails.primary_care_physician}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="specialist-hdr">Specialist</th> \
+            <td class="plan-detail">${planDetails.specialist}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="er-hdr">Emergency Room</th> \
+            <td class="plan-detail">${planDetails.emergency_room}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="gen-drug-hdr">Generic Drug</th> \
+            <td class="plan-detail">${planDetails.generic_drugs}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="urg-care-hdr">Urgent Care</th> \
+            <td class="plan-detail">${planDetails.urgent_care}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="indiv-deduc-hdr">Individual Deductible</th> \
+            <td class="plan-detail">${planDetails.individual_medical_deductible}</td> \
+          </tr> \
+          <tr> \
+            <th scope="row" class="indiv-med-moop-hdr">Individual Max Out-of-Pocket</th> \
+            <td class="plan-detail">${planDetails.individual_medical_moop}</td> \
+          </tr> \
 
-            <tr> \
-              <td colspan="2" align="center"> \
+          <tr> \
+            <td colspan="2" align="center"> \
               <div class="custom-control custom-checkbox"> \
                 <input type="checkbox" class="custom-control-input" id="${planDetails.id}"> \
-                <label class="custom-control-label" for="customCheck1">Select Plan</label> \
+                <label class="custom-control-label" for="${planDetails.id}">Check this custom checkbox</label> \
               </div> \
-              
-              </td> \
-            </tr> \
-          </tbody> \
-        </table> \
-      </div> \
-      `);
-  
+            </td> \
+          </tr> \
+        </tbody> \
+      </table> \
+    `);
+
     allTablePlans.push(tablePlan);
   };
-  displayPlanDiv.html(allTablePlans);
+  // get contents of elements in indivPlan, convert allTablesPlans to html
+  indivPlan.html(allTablePlans);
 
   // display shows once function runs to generate tables
   $('#display-plans-div').show();
