a62b7d549c38dbec7fec644e10b550906c813fdb
Sarah Tao <satao.94@gmail.com>
2020-03-05 01:05:17 +0000

    clean code, remove "

--- static/js/plans.js
+++ static/js/plans.js
@@ -13,74 +13,74 @@ const previousButton = $('#click-previous-plans');
 // standard table showing all plan's information including deductibles
 const tableDisplay = (planDetails) => {
 
-  const tablePlan = (`\
-  <div class="indiv-plan"> \
-    <table class="table table-hover table-sm"> \
-      <thead> \
-        <tr> \
-          <th scope="col" id="display-name">${planDetails.display_name}</th> \
-        </tr> \
-      </thead> \
-      <tbody> \
-        <tr> \
-          <th scope="row" class="plan-id-hdr">Plan ID</th> \
-          <td class="plan-detail-id">${planDetails.id}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="carrier-hdr">Carrier</th> \
-          <td class="plan-detail-carrier">${planDetails.carrier_name}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="type-hdr">Plan Type</th> \
-          <td class="plan-detail-type">${planDetails.plan_type}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="pcp-hdr">Primary Care Physician</th> \
-          <td class="plan-detail-pcp">${planDetails.pcp}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="specialist-hdr">Specialist</th> \
-          <td class="plan-detail-s">${planDetails.specialist}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="er-hdr">Emergency Room</th> \
-          <td class="plan-detail-er">${planDetails.emerg_rm}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="gen-drug-hdr">Generic Drug</th> \
-          <td class="plan-detail-gd">${planDetails.gen_drug}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="urg-care-hdr">Urgent Care</th> \
-          <td class="plan-detail-uc">${planDetails.urg_care}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="indiv-deduc-hdr">Individual Deductible</th> \
-          <td class="plan-detail-imd">${planDetails.med_deduct}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="indiv-med-moop-hdr">Individual Max Out-of-Pocket</th> \
-          <td class="plan-detail-moop">${planDetails.med_moop}</td> \
-        </tr> \
-        <tr> \
-          <th scope="row" class="premium-hdr">Premium</th> \
-          <td class="plan-detail-premium">${planDetails.premium}</td> \
-        </tr> \
-        <tr> \
-          <td colspan="2" align="center"> \
-          <div class="form-check"> \
-            <input class="form-check-input" type="checkbox" \
-                                            value="plan_id"" \
-                                            name="${planDetails.id}"> \
-            <label class="form-check-label" for="${planDetails.id}"> \
-              Default checkbox \
-            </label> \
-          </div> \
-          </td> \
-        </tr> \
-      </tbody> \
-    </table> \
-  </div> \
+  const tablePlan = (`
+  <div class="indiv-plan"> 
+    <table class="table table-hover table-sm"> 
+      <thead> 
+        <tr> 
+          <th scope="col" id="display-name">${planDetails.display_name}</th> 
+        </tr> 
+      </thead> 
+      <tbody> 
+        <tr> 
+          <th scope="row" class="plan-id-hdr">Plan ID</th> 
+          <td class="plan-detail-id">${planDetails.id}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="carrier-hdr">Carrier</th> 
+          <td class="plan-detail-carrier">${planDetails.carrier_name}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="type-hdr">Plan Type</th> 
+          <td class="plan-detail-type">${planDetails.plan_type}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="pcp-hdr">Primary Care Physician</th> 
+          <td class="plan-detail-pcp">${planDetails.pcp}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="specialist-hdr">Specialist</th> 
+          <td class="plan-detail-s">${planDetails.specialist}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="er-hdr">Emergency Room</th> 
+          <td class="plan-detail-er">${planDetails.emerg_rm}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="gen-drug-hdr">Generic Drug</th> 
+          <td class="plan-detail-gd">${planDetails.gen_drug}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="urg-care-hdr">Urgent Care</th> 
+          <td class="plan-detail-uc">${planDetails.urg_care}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="indiv-deduc-hdr">Individual Deductible</th> 
+          <td class="plan-detail-imd">${planDetails.med_deduct}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="indiv-med-moop-hdr">Individual Max Out-of-Pocket</th> 
+          <td class="plan-detail-moop">${planDetails.med_moop}</td> 
+        </tr> 
+        <tr> 
+          <th scope="row" class="premium-hdr">Premium</th> 
+          <td class="plan-detail-premium">${planDetails.premium}</td> 
+        </tr> 
+        <tr> 
+          <td colspan="2" align="center"> 
+          <div class="form-check"> 
+            <input class="form-check-input" type="checkbox" 
+                                            value="plan_id"" 
+                                            name="${planDetails.id}"> 
+            <label class="form-check-label" for="${planDetails.id}"> 
+              Default checkbox 
+            </label> 
+          </div> 
+          </td> 
+        </tr> 
+      </tbody> 
+    </table> 
+  </div> 
 `);
 
 return tablePlan;
@@ -106,10 +106,8 @@ const individualMedPlan = (medicalPlans) => {
   };
 
   for (let data in sliceCounter) {
-
     const planDetails = sliceCounter[data];
     const tablePlan = tableDisplay(planDetails);
-
     allTablePlans.push(tablePlan);
   };
   // convert allTablesPlans to html
@@ -174,7 +172,6 @@ moreButton.on('click', individualMedPlan, (evt) => {
 
 // click event to generate button allowing users to see previous plans
 previousButton.on('click', individualMedPlan, (evt) => {
-  
   // counter show previous 5 tables every click
   begin -= 5;
   end -= 5;
